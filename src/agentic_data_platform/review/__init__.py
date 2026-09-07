c2d8cd45d7e5ec4d281768399912767884b163a0
rrahul0904 <rrahul0904@users.noreply.github.com>
2026-09-07 02:01:06 -0400

    feat: expose review and delivery control surface

--- src/agentic_data_platform/review/__init__.py
+++ src/agentic_data_platform/review/__init__.py
@@ -1,3 +1,18 @@
-from .dbt import format_review_body, review_dbt_changes
+from .dbt import (
+    change_impact,
+    deployment_risk,
+    format_review_body,
+    recommended_tests,
+    review_dbt_changes,
+)
+from .delivery import deliver_github_review, deliver_gitlab_review
 
-__all__ = ["format_review_body", "review_dbt_changes"]
+__all__ = [
+    "change_impact",
+    "deliver_github_review",
+    "deliver_gitlab_review",
+    "deployment_risk",
+    "format_review_body",
+    "recommended_tests",
+    "review_dbt_changes",
+]
