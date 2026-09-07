d0f0ebeda7d7e7141b617ab4be97f9c77cf47d67
rrahul0904 <rrahul0904@users.noreply.github.com>
2026-09-06 23:40:22 -0400

    fix: correct remediation package exports

--- src/agentic_data_platform/remediation/__init__.py
+++ src/agentic_data_platform/remediation/__init__.py
@@ -1,1 +1,13 @@
-from .proposals import propose_airflow_retry, propose_dbt_tests, propose_quality_rule, propose_sql_repair\n\n__all__ = ['propose_airflow_retry', 'propose_dbt_tests', 'propose_quality_rule', 'propose_sql_repair']\n
+from .proposals import (
+    propose_airflow_retry,
+    propose_dbt_tests,
+    propose_quality_rule,
+    propose_sql_repair,
+)
+
+__all__ = [
+    "propose_airflow_retry",
+    "propose_dbt_tests",
+    "propose_quality_rule",
+    "propose_sql_repair",
+]
