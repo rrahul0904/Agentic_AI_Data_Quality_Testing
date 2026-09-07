54a8238b21381f3a33bf3e627e71d6f235039298
rrahul0904 <rrahul0904@users.noreply.github.com>
2026-09-07 01:36:03 -0400

    feat: expose provider control plane

--- src/agentic_data_platform/providers/__init__.py
+++ src/agentic_data_platform/providers/__init__.py
@@ -1,13 +1,36 @@
 from .anthropic import AnthropicProvider
 from .base import Provider, ProviderRequest, ProviderResponse, ToolCall, Usage
+from .control import (
+    MODEL_STATUSES,
+    OUTPUT_TOKEN_FLOOR,
+    ModelCatalog,
+    ModelRecord,
+    ProviderAuthError,
+    ProviderBudgetError,
+    ProviderControlError,
+    configured_auth,
+    family_vendor,
+    load_catalog_snapshot,
+    normalize_messages,
+    output_token_budget,
+    parse_catalog,
+    provider_auth_status,
+)
 from .mock import ScriptedProvider
 from .openai_compatible import OpenAICompatibleProvider
 from .registry import ProviderRegistry, ProviderSpec
 
 __all__ = [
     "AnthropicProvider",
+    "MODEL_STATUSES",
+    "ModelCatalog",
+    "ModelRecord",
     "OpenAICompatibleProvider",
+    "OUTPUT_TOKEN_FLOOR",
     "Provider",
+    "ProviderAuthError",
+    "ProviderBudgetError",
+    "ProviderControlError",
     "ProviderRegistry",
     "ProviderRequest",
     "ProviderResponse",
@@ -15,4 +38,11 @@ __all__ = [
     "ScriptedProvider",
     "ToolCall",
     "Usage",
+    "configured_auth",
+    "family_vendor",
+    "load_catalog_snapshot",
+    "normalize_messages",
+    "output_token_budget",
+    "parse_catalog",
+    "provider_auth_status",
 ]
