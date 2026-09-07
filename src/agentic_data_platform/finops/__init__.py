57a7e46f332aed632bd6e1ba1374feeea18adbc4
rrahul0904 <rrahul0904@users.noreply.github.com>
2026-09-07 01:12:39 -0400

    feat: expose FinOps engine

---
+++ src/agentic_data_platform/finops/__init__.py
@@ -0,0 +1,23 @@
+from .engine import (
+    cost_summary,
+    expensive_queries,
+    full_finops_report,
+    idle_resources,
+    query_errors,
+    query_history,
+    query_patterns,
+    warehouse_advisor,
+    warehouse_usage,
+)
+
+__all__ = [
+    "cost_summary",
+    "expensive_queries",
+    "full_finops_report",
+    "idle_resources",
+    "query_errors",
+    "query_history",
+    "query_patterns",
+    "warehouse_advisor",
+    "warehouse_usage",
+]
