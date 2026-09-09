ca8b34602444f531fe65d8962a0c1caf04fde152
rrahul0904 <rrahul0904@users.noreply.github.com>
2026-09-09 14:58:30 -0400

    feat: export Snowpark ML registry APIs

---
+++ src/agentic_data_platform/ml/__init__.py
@@ -0,0 +1,13 @@
+from .registry import (
+    EmbeddedModelRegistryRuntime,
+    SnowflakeModelRegistryAdapter,
+    plan_log_model,
+    plan_model_lifecycle,
+)
+
+__all__ = [
+    "EmbeddedModelRegistryRuntime",
+    "SnowflakeModelRegistryAdapter",
+    "plan_log_model",
+    "plan_model_lifecycle",
+]
