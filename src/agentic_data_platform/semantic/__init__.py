0e9f6c0e39f8a7013d3fcaa315e05d6c954d68be
rrahul0904 <rrahul0904@users.noreply.github.com>
2026-09-09 16:04:32 -0400

    feat: export dbt and LookML semantic adapters

--- src/agentic_data_platform/semantic/__init__.py
+++ src/agentic_data_platform/semantic/__init__.py
@@ -1,3 +1,5 @@
+from .dbt_adapter import ingest_dbt_semantic_project
+from .lookml_adapter import ingest_lookml_project
 from .cortex import CortexAnalystAdapter, build_analyst_request
 from .evaluation import canonical_sql, evaluate_batch, evaluate_candidate, sql_tables
 from .registry import SemanticElement, SemanticRegistry
@@ -5,6 +7,8 @@ from .snowflake import SnowflakeSemanticAdapter
 
 __all__ = [
     "CortexAnalystAdapter",
+    "ingest_dbt_semantic_project",
+    "ingest_lookml_project",
     "SemanticElement",
     "SemanticRegistry",
     "SnowflakeSemanticAdapter",
