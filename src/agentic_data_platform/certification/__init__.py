f47d2094c7179713026ffd370665e22b2e3d813c
rrahul0904 <rrahul0904@users.noreply.github.com>
2026-09-10 10:01:44 -0400

    fix(coco): export operator certification status from package

--- src/agentic_data_platform/certification/__init__.py
+++ src/agentic_data_platform/certification/__init__.py
@@ -1,3 +1,9 @@
+from .coco import coco_certification_status
 from .framework import CertificationRunner, CertificationStatus, WAREHOUSES
 
-__all__ = ["CertificationRunner", "CertificationStatus", "WAREHOUSES"]
+__all__ = [
+    "CertificationRunner",
+    "CertificationStatus",
+    "WAREHOUSES",
+    "coco_certification_status",
+]
